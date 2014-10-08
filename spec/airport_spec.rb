9b8151c317d83f993cb17e6a88d06d03ce4f4fac
Nicolai <nicolai@mmg.io>
2014-10-08 14:46:07 +0100

    Additional tests in progress

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -9,7 +9,7 @@ describe Airport do
 	
 	context 'Airport Capacity' do
 
-	it "allows default capacity on initialization" do
+	it 'allows default capacity on initialization' do
 		expect(airport.capacity).to eq 15
 
 		end
@@ -17,18 +17,37 @@ describe Airport do
 	end
 
 
-		context "taking off and landing" do
+		context 'taking off and landing' do
 
 			before(:each) do
 				allow(airport).to receive(:stormy?) {false}
 			end
 
-			it "allows a plane to land" do
+			it 'allows a plane to land' do
 				plane = double :plane, landed!: self, flying?: true
 				airport.dock(plane)
 				expect(airport.airport_planes).to include (plane)
-		end
+			end
 
-	end
+			it 'allows a plane to have the landed! status' do
+				plane = double :plane, flying?: true
+				expect(plane).to receive(:landed!)
+				airport.dock(plane)
+			end
+
+			it 'allows a plane to take off' do
+				plane = double :plane, landed!: self, flying?: true, takeoff!: self
+				airport.dock(plane)
+				airport.liftoff(plane)
+				expect(airport.airport_planes).not_to include (plane)
+			end
+
+			it 'allows a plane to have the flying status' do
+				plane = double :plane
+				expect(plane).to receive(:takeoff!)
+				airport.dock(plane)
+			end
+			
+		end
 
 end
